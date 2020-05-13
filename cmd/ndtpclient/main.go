633b769751c96bcdf0b12dd66633b2148fb00521
egorban <gorban@satissoft.ru>
2020-05-13 22:30:00 +0300

    fix

--- cmd/ndtpclient/main.go
+++ cmd/ndtpclient/main.go
@@ -12,7 +12,7 @@ func main() {
 	var time int
 	flag.StringVar(&serverAddress, "s", "localhost:9000", "server address (e.g. 'localhost:9000')")
 	flag.IntVar(&terminalID, "i", 1, "terminal ID (e.g. '1')")
-	flag.IntVar(&terminalID, "t", 20, "time period seconds (e.g. '20')")
+	flag.IntVar(&time, "t", 20, "time period seconds (e.g. '20')")
 	flag.Parse()
-	ndtpclient.Start(serverAddress, terminalID)
+	ndtpclient.Start(serverAddress, terminalID, time)
 }
